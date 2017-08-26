d99adb7404ebb7d4b6da10a74475703ba5b70c3c
Juri <juri.l@me.com>
2017-08-27 01:38:48 +0300

    Add files via upload
    changes during section 3 and 4.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+  return [
+    {title: 'Wilkinson Trail'},
+    {title: "It's not over until i win"},
+    {title: 'Tremendous holidays'},
+    {title: 'Time to make things'},
+    {title: 'Motivate your self'}
+  ];
+};
+// step 1 create the reducer
